5b148f14dee45c13daa39bc8a199d6405aab5504
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-10-14 22:15:34 +0200

    create 'ast' package

---
+++ ast/ast.go
@@ -0,0 +1,15 @@
+package ast
+
+import "avrasm/token"
+
+type Node interface {}
+
+type Instruction struct {
+    Mnemonic token.Token
+    Operands []Operand
+}
+
+type Operand interface {}
+
+type Register token.Token
+
